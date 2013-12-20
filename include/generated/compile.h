2a731f63cb645246efe26dc9e2545a9b6371f3e5
yazidkucrit <yazid.kucrit@gmail.com>
2013-12-20 18:21:42 +0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Fri Dec 20 11:22:19 WIB 2013"
+#define LINUX_COMPILE_BY "yazid"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
